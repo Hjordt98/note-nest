a0fbf9381c3b904a16bb6671d84299954c5e2095
Hjordten <andr314e@stud.kea.dk>
2025-06-02 09:56:02 +0200

    Tutorial done

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="flex bg-zinc-900 h-screen">
         <!-- sidebar start -->
-        <div class="bg-black w-[338px] p-8">
-            <Logo />
-
+        <div class="bg-black w-[338px] p-8 flex flex-col overflow-scroll">
+            <div><Logo /></div>
             <!-- today main container start -->
-             <div>
+             <div class="flex-grow">
                 <p class="text-xs font-bold text-[#C2C2C5] mt-12 mb-4">Today</p>
                 <div class="ml-2 space-y-2">
                 <div v-for="note in todaysNotes"
@@ -13,18 +12,16 @@
                 :class="{ 'bg-[#A1842C]': note.id === selectedNote.id,
                           'hover:bg-[#A1842C]/50': note.id !== selectedNote.id
                  }"
-                @click="selectedNote = note"
+                @click="setNote(note)"
                 >    
                     <h3 class="text-sm font-bold text-[#F4F4F5] truncate">
                         {{ note.text.substring(0, 50) }}
                     </h3>
                     <div class="leading-none truncate text-[#D6D6D6]">
                         <span class="text-xs text-[#F4F4F5] mr-4">{{ 
-                        new Date(note.updatedAt).toDateString() ===
-                        new Date().toDateString()
-                        ? 'Today'
-                        : new Date(note.updatedAt).toLocaleDateString() }}</span>
-                        <span class="text-xs text-[#D6D6D6]">... {{ note.text.substring(50, 100)}}</span>
+                        new Date(note.updatedAt).toLocaleDateString() }}</span>
+                        <span v-if="note.text.length > 50" class="text-xs text-[#D6D6D6]"
+                            > {{ note.text.substring(50, 100)}}</span>
                     </div>
                 </div>
             </div>
@@ -50,7 +47,8 @@
                         new Date().toDateString()
                         ? 'Today'
                         : new Date(note.updatedAt).toLocaleDateString() }}</span>
-                        <span class="text-xs text-[#D6D6D6]">... {{ note.text.substring(50, 100)}}</span>
+                        <span  v-if="note.text.length > 50" class="text-xs text-[#D6D6D6]"
+                            >... {{ note.text.substring(50, 100)}}</span>
                     </div>
                 </div>
             </div>
@@ -66,7 +64,7 @@
                 :class="{ 'bg-[#A1842C]': note.id === selectedNote.id,
                           'hover:bg-[#A1842C]/50': note.id !== selectedNote.id
                  }"
-                @click="selectedNote = note"
+                @click="setNote(note)"
                 >    
                     <h3 class="text-sm font-bold text-[#F4F4F5] truncate">
                         {{ note.text.substring(0, 50) }}
@@ -77,7 +75,8 @@
                         new Date().toDateString()
                         ? 'Today'
                         : new Date(note.updatedAt).toLocaleDateString() }}</span>
-                        <span class="text-xs text-[#D6D6D6]">... {{ note.text.substring(50, 100)}}</span>
+                        <span  v-if="note.text.length > 50" class="text-xs text-[#D6D6D6]"
+                            >... {{ note.text.substring(50, 100)}}</span>
                     </div>
                 </div>
             </div>
@@ -92,29 +91,40 @@
         <!-- note container start -->
          <div class="w-full flex flex-col">
             <div class="flex justify-between w-full items-start p-8">
-                <button class="inline-flex items-center text-xs text-[#C2C2C5] hover:text-white font-bold space-x-2">
+                <button class="inline-flex items-center text-xs text-[#C2C2C5] hover:text-white
+                               font-bold space-x-2"
+                               @click="createNewNote"
+                               >
                     <PencilIcon />
                     <span>Create Note</span>
                 </button>
-                <button><TrashIcon class="text-[#6D6D73] hover:text-white" /></button>
+                <button @click="deleteNote"><TrashIcon class="text-[#6D6D73] hover:text-white" /></button>
             </div>
             
             <div class="max-w-[438px] mx-auto w-full flex-grow flex flex-col">
              <p class="text-[#929292] font-playfair">
                {{ new Date(selectedNote.updatedAt).toLocaleDateString() }}
              </p>
-             <textarea 
+             <textarea
+             ref="textarea" 
              v-model="updatedNote"
              name="note" 
              id="note"
              class="text-[#D4D4D4] my-4 font-playfair w-full bg-transparent
              focus:outline-none resize-none flex-grow"
-             @input="debouncedFn" 
+             @input="() => {
+                debouncedFn()
+                selectedNote.text = updatedNote
+             }"
               >
             </textarea>
-           
-                
             </div>
+
+            <button class="text-zinc-400 hover:text-white text-sm font-bold absolute right-0 bottom-0 p-8"
+            @click="logout"
+            >
+                logout
+            </button>
          </div>
         <!-- note container end -->
     </div>
@@ -126,10 +136,63 @@
    const updatedNote = ref('')
    const notes = ref([])
    const selectedNote = ref({})
+   const textarea = ref(null)
 
    definePageMeta({
     middleware: ['auth']
    })
+
+   async function deleteNote() {
+    try {
+        await $fetch(`api/notes/${selectedNote.value.id}`, {
+            method: 'DELETE',
+        })
+        
+        notes.value = notes.value.filter(note => note.id !== selectedNote.value.id)
+        
+        if (notes.value.length > 0) {
+            selectedNote.value = notes.value[0]
+            updatedNote.value = selectedNote.value.text
+        } else {
+            await createNewNote()
+        }
+        
+        textarea.value.focus()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+   function setNote(note) {
+       selectedNote.value = note
+       updatedNote.value = note.text
+                
+   }
+
+   async function logout() {
+    const jwtCookie = useCookie('NoteNestJWT')
+    jwtCookie.value = null
+    navigateTo('/login')
+   }
+
+   async function createNewNote() {
+    try{
+         const newNote =await $fetch('/api/notes', {
+            method: 'POST',
+        })
+
+        notes.value.unshift(newNote)
+        selectedNote.value = notes.value [0]
+        updatedNote.value = ''
+        textarea.value.focus()
+        console.log(response)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
 
    const debouncedFn = useDebounceFn(async () => {
     await updateNote()
@@ -183,8 +246,16 @@
    onMounted(async () => {
     notes.value = await $fetch('/api/notes')
 
+    notes.value.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
+
     if (notes.value.length > 0 ) selectedNote.value = notes.value[0] 
+    else {
+        await createNewNote()
+        selectedNote.value = notes.value[0]
+    }
         
     updatedNote.value = selectedNote.value.text
+
+    textarea.value.focus()
    })
 </script>
---
+++ server/api/notes/[id].delete.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+
+export default defineEventHandler(async (event) => {
+try {
+    const prisma = new PrismaClient()
+    const id = await getRouterParam(event, 'id')
+    const body = await readBody(event)
+
+    const cookies = parseCookies(event)
+    const token = cookies.NoteNestJWT
+
+    if(!token) {
+        throw createError({
+            statuscode: 400,
+            statusMessage: 'Not authorized to update',
+        })
+    }
+
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET)
+
+    const noteTryingToUpdate = await prisma.note.findUnique({
+        where: {
+            id: Number(id),
+        },
+    })
+
+    if (!noteTryingToUpdate) {
+        throw createError({
+            statuscode: 404,
+            statusMessage: 'Note does not exist',
+        })
+    }
+
+    if (noteTryingToUpdate.userId !== decodedToken.id) {
+        throw createError({
+            statuscode: 403,
+            statusMessage: 'Does not have permission to update this note',
+        })
+    }
+
+    if(!noteTryingToUpdate) {
+        throw createError({
+            
+        })
+    }
+
+    console.log(id)
+
+    await prisma.note.delete({
+      where: {
+        id: Number(id),
+      },
+    })
+    console.log(body)
+} catch (err) {
+    console.log(err)
+    }
+})
